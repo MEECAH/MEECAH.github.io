76748e944dd8c7173bec9d158a781a7fe388e12d
MEECAH <micahjhaycraft@gmail.com>
2020-06-16 18:41:58 -0400

    working aframe and ar.js demos

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var fs = require('fs')
+var https = require('https')
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
+// viewed at http://localhost:8000
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
-});
+https.createServer({
+  key: fs.readFileSync(__dirname + '/48871598_192.168.1.28000.key'),
+  cert: fs.readFileSync(__dirname + '/48871598_192.168.1.28000.cert')
+}, app)
+.listen(8000, function () {
+  console.log('Example app listening on port 8000! Go to https://localhost:8000/')
+})
